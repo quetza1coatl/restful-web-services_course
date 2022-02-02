4c9d3f48d52e1a3418b181b1307848d3c2e27f3e
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2022-02-02 20:26:18 +0300

    add i18n

--- src/main/java/com/quetzalcoatl/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/quetzalcoatl/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,20 @@
 package com.quetzalcoatl.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -20,5 +29,19 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    /* with header injection */
+//    @GetMapping(path = "/hello-world/i18n")
+//    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+//        return messageSource.getMessage("greeting", null, "default message", locale);
+//    }
+
+    /* with LocaleContextHolder */
+    @GetMapping(path = "/hello-world/i18n")
+    public String helloWorldInternationalized() {
+        return messageSource.getMessage(
+                "greeting", null, "default message", LocaleContextHolder.getLocale()
+        );
     }
 }
